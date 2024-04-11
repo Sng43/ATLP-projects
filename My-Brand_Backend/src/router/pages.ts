ba1d70ccb57e655f5702c979b583b414e8d9d5a8
Sng43 <s.kabare1@alustudent.com>
2024-04-11 10:14:05 +0200

    front-end

--- My-Brand_Backend/src/router/pages.ts
+++
@@ -1,7 +0,0 @@
-import { startingPoint } from '../controllers/pages';
-import express from 'express';
-import router from 'router';
-
-export default (router: express.Router) => {
-    router.get("/home",startingPoint)
-}
