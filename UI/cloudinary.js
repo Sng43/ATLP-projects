3ca2deedcf455e996061a773ad610d6af49608fd
Sng43 <s.kabare1@alustudent.com>
2024-04-30 10:59:36 +0200

    redeem

--- UI/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const tcloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "dfun5i2l3",
-  api_key: "751635192886752",
-  api_secret: "y2z2uakqMNptC0cco8cP5xpsRJM",
-});
-
-export default cloudinary;
