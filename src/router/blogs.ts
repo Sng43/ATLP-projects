00e2bfde92d102f257e228d25e2de75c548def78
Sng43 <s.kabare1@alustudent.com>
2024-04-30 14:44:15 +0200

    main

--- src/router/blogs.ts
+++ src/router/blogs.ts
@@ -44,7 +44,7 @@ export default (router: express.Router) => {
      *         '200':
      *           description: Successful response
      */
-    router.get("/blogs", isAuthenticated,getAllBlogs)
+    router.get("/blogs",getAllBlogs)
 
     /**
      * @openapi
